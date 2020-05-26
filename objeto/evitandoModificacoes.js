8f0ce6dc778537fc526cb1e315f8fbfbc8b4f2c0
Edson Batista <jr-lol@live.com>
2020-05-26 20:57:56 -0300

    evitando modificações

---
+++ objeto/evitandoModificacoes.js
@@ -0,0 +1,28 @@
+// Object.preventExtensions - nao permite inclusões de novos atributos
+const produto = Object.preventExtensions({
+  nome: "Qualquer",
+  preco: 1.99,
+  tag: "Promoção",
+});
+
+console.log("Extensível: ", Object.isExtensible(produto));
+
+produto.nome = "Borracha";
+produto.descricao = "Borra escolar";
+delete produto.tag;
+
+console.log(produto);
+
+// Object.seal - permite alterar, mas não incluir nem excluir
+const pessoa = { nome: "Juliana", idade: 35 };
+Object.seal(pessoa);
+
+console.log("Selado:", Object.isSealed(pessoa));
+
+pessoa.sobrenome = "Silva";
+delete pessoa.nome;
+pessoa.idade = 29;
+
+console.log(pessoa);
+
+// Object.freeze - selado + valores constantes
